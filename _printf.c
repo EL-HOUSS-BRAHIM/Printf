e60851cc6c1a295243f8c2faff961bbdd6a50c2c
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-09-19 15:12:34 -0700

    test

--- _printf.c
+++ _printf.c
@@ -1,10 +1,13 @@
+#include "main.h"
+#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+
 /**
- * _printf - Custom printf implementation
- * @format: The format string
- *
- * Return: The number of characters printed (excluding the null byte)
+ * _printf - Custom printf function
+ * @format: Format string
+ * @...: Variable number of arguments
+ * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
@@ -16,43 +19,66 @@ while (*format)
 if (*format == '%')
 {
 format++;
-switch (*format)
-{
-case 'c':
+if (*format == 'c')
 {
 char c = va_arg(args, int);
 write(1, &c, 1);
 count++;
-break;
 }
-case 's':
+else if (*format == 's')
 {
 char *str = va_arg(args, char *);
 if (str == NULL)
 str = "(null)";
-while (*str)
+int len = 0;
+while (str[len])
+len++;
+write(1, str, len);
+count += len;
+}
+else if (*format == 'd' || *format == 'i')
 {
-write(1, str, 1);
-str++;
-count++;
-}
-break;
-}
-case '%':
+int num = va_arg(args, int);
+int temp = num;
+int digits = 0;
+if (num == 0)
 {
-write(1, "%", 1);
+write(1, "0", 1);
 count++;
-break;
 }
-default:
-write(1, &(*format), 1);
+else if (num < 0)
+{
+write(1, "-", 1);
 count++;
-break;
+num = -num;
+temp = -temp;
+}
+while (temp > 0)
+{
+temp /= 10;
+digits++;
+}
+while (digits > 0)
+{
+int divisor = 1;
+for (int i = 1; i < digits; i++)
+divisor *= 10;
+char digit = (num / divisor) + '0';
+write(1, &digit, 1);
+count++;
+num %= divisor;
+digits--;
 }
 }
 else
 {
-write(1, &(*format), 1);
+write(1, "%", 1);
+count++;
+}
+}
+else
+{
+write(1, format, 1);
 count++;
 }
 format++;
